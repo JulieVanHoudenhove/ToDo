5341a6b03dd930dd3d6eab01f814a37ef783cc72
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-05-05 09:56:43 +0200

    form à peu près bien

--- src/Form/TacheType.php
+++ src/Form/TacheType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Tache;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,33 @@ class TacheType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('priority')
-            ->add('checked')
-            ->add('date')
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Je dois faire...',
+                ],
+            ])
+            ->add('priority', ChoiceType::class, [
+                'label' => false,
+                'choices' => [
+                    '🔴  Très Urgent' => '1',
+                    '🟠  Urgent' => '2',
+                    '🟡  Peu Urgent' => '3',
+                    '🟢  Pas Urgent' => '4',
+                ],
+            ])
+            ->add('checked', HiddenType::class, [
+                'required' => false,
+                'data' => '0',
+            ])
+            ->add('date', DateType::class, [
+                'label' => '🗓️',
+                'widget' => 'single_text',
+                'attr' => ['class' => 'js-datepicker'],
+                'html5' => false,
+                'format' => 'dd MM yyyy',
+                'data' => new \DateTime('now'),
+            ])
         ;
     }
 
