f338b102f5bc3964fa5a7664b1c0e8fda486621a
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-05-09 12:01:09 +0200

    alpine + axios fonctionnel

--- src/Entity/Tache.php
+++ src/Entity/Tache.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TacheRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TacheRepository::class)]
+#[ApiResource]
 class Tache
 {
     #[ORM\Id]
